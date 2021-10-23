534a11a4e22fe2bfe1e2f4f3d0d5ad9593967e33
Gunzelb <brandongunzel@gmail.com>
2021-10-23 14:17:08 -0700

    fixed mysql connection

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,8 @@ require("dotenv").config();
 
 const db = mysql.createConnection(
   {
-    host: process.env.DB_NAME,
+    host: "localhost",
+    port: 3306,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: "employee_db",
--- server.js
+++ server.js
@@ -8,6 +8,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 const start = async () => {
+  let check = menu();
+  console.log(check);
   if (await menu()) {
     start();
   } else {
